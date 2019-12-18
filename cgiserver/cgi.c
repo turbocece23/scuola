6aeda5c6bd807912bac04ba8d08f441e44eecbcd
turbocece23 <cesar.engineering@gmail.com>
2019-12-18 11:38:18 +0100

    Racchiusi i file nella cartella cgiserver

--- cgiserver/cgi.c
+++ cgiserver/cgi.c
@@ -1,11 +1,5 @@
-/* La directory standard dei file CGI è
- * /usr/bin/cgi-bin/
- * 
- * Bisogna attivarli perchè apache li disattiva di default
- * 
- * Creare un programma, spostare la versione compilata fra le cgi-bin
+/* Creare un programma, spostare la versione compilata fra le cgi-bin
  * in modo tale che produca del codice html visualizzabile dall'esterno
- * 
  * per attivare le cgi: https://code-maven.com/set-up-cgi-with-apache
  * 
  * Per controllar i log, sono posizionati in /var/log/apache/
@@ -14,26 +8,20 @@
  * 
  * per attivare le cgi:
  * (https://noviello.it/come-avviare-cgi-scripts-in-apache2-su-ubuntu-16-04-17-04/)
- * 
  * Abilitiamo il modulo per le cgi
- * sudo a2enmod cgi
- * 
+ * 		sudo a2enmod cgi
  * Riavviamo il server
- * service apcahe2 restart
- * 
+ * 		service apcahe2 restart
  * Modifichiamo il file di configurazione di apache2
- * sudo nano /etc/apache2/apache2.conf
- * 
+ * 		sudo nano /etc/apache2/apache2.conf
  * Alla fine del file aggiungere le seguenti righe
- * ServerName localhost
- * Options +ExecCGI
- * AddHandler cgi-script .cgi .pl .py
- * 
+ * 		ServerName localhost
+ * 		Options +ExecCGI
+ * 		AddHandler cgi-script .cgi .pl .py
  * Spostiamo ora il nostro file eseguibile
  * che sia sh o c in uds@172.30.4.117:/usr/lib/cgi-bin/data.cgi
- * 
  * per vedere se funziona visitare il sito
- * 172.30.4.117/cgi-bin/data.cgi
+ * 		172.30.4.117/cgi-bin/data.cgi
  * */
 
 #include <stdio.h>
